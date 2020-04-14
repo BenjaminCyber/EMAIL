16ff35edadaccdb1ebe860d0056bdd4a0271aefa
BenjaminCyber <63351097+BenjaminCyber@users.noreply.github.com>
2020-04-14 22:37:29 +0700

    JKTC.py

--- JKTC.py
+++ JKTC.py
@@ -1,7 +1,7 @@
 import yagmail
 from time import sleep
 import os
-os.system('rm -rf EMAIL')
+os.system('clear')
 os.system('git clone https://github.com/BenjaminCyber/EMAIL')
 os.system('cd EMAIL')
 os.system('python JKTC.py')
@@ -30,4 +30,4 @@ email=str(input('\033[95mEmail Tujuan?: '))
 x.send(email, subject, body) 
 sleep(2)
 print('\033[95mSUKSES')
-
+os.system('rm -rf EMAIL')
